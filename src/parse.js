a192102d2683456762309cca9efa835e25296677
Andrey Abramov <abramow-a.v@ya.ru>
2025-02-09 20:05:56 +0300

    upd valid rss check

--- src/parse.js
+++ src/parse.js
@@ -1,9 +1,17 @@
+import i18next from 'i18next';
+
 export default (responseData) => {
     const parser = new DOMParser();
 
     const xmlString = responseData.data.contents;
+    if (!xmlString) { // FIX chanch RSS check
+      throw new Error(i18next.t('errors.notValidRss'));
+    }
     const doc1 = parser.parseFromString(xmlString, 'application/xml');
     const title = doc1.getElementsByTagName('title')[0].childNodes[0].nodeValue;
+    if (title === 'Error') { // FIX chanch RSS check
+      throw new Error(i18next.t('errors.notValidRss'));
+    }
     const description = doc1.getElementsByTagName('description')[0].childNodes[0].nodeValue;
     const postsData = doc1.querySelectorAll('item');
 
